75e0cc2c2dea69e2e916c6594078f91fd6e50d39
kshtijj <kshitijrajgude@gmail.com>
2022-08-19 18:44:47 +0530

    learn about Variables (declaring and using)

---
+++ Variables/Variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+//declaring integer, float, character, string, boolean
+int myNum = 15;
+double myFloatNum = 420.69;
+char myLetter = 'D';
+string myString = "Hello World";
+bool myBoolean = true;
+
+int main(){
+    // displaying variables
+    int myAge = 15;
+    cout << "I am " << myAge << " Years Old" << endl;
+
+    // adding variables
+    int x, y;
+    x = 10;
+    y = 20;
+    cout << x + y << endl;
+}
