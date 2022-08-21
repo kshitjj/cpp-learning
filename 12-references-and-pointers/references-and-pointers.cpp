fd789ed072b6609ff6c26b938c2e8fa8ca76f9e5
kshtijj <kshitijrajgude@gmail.com>
2022-08-21 19:10:22 +0530

    accessing with memory address

--- 12-references-and-pointers/references-and-pointers.cpp
+++ 12-references-and-pointers/references-and-pointers.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
+	// Creating a reference variable 
+	string food = "Pizza";
+	string &meal = food;
+	cout << meal << endl;	
+
 	// getting the memory addresses of the variable
 	string greeting = "Hello!";
 	cout << &greeting << endl;
@@ -11,5 +16,10 @@ int main(){
 	// NOTE : use string if the address you want is of string type and int if it's of int type.
 	string* ptr = &greeting;
 	cout << ptr << endl;
+
+	// accessing the memory address of food and editing it
+	cout << "Accessing the value of Greeting with memory address " << *ptr << endl;
+	*ptr = "Good Morning";
+	cout << "Changing the value of ptr and outputting it: " << *ptr << endl;
 
 }
